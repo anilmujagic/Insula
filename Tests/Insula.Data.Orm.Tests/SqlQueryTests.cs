c6431c5ec1867859a0595fadaf6e594545173287
Anil Mujagic <anil.mujagic@gmail.com>
2012-09-08 07:53:09 +0200

    Restructure unit test projects

---
+++ Tests/Insula.Data.Orm.Tests/SqlQueryTests.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Xunit;
+
+namespace Insula.Data.Orm.Tests
+{
+    public class SqlQueryTests
+    {
+        public class Where_ByObject_Method
+        {
+            [Fact]
+            public void CreatesWhereClauseFromPassedAnonymousObject()
+            {
+
+            }
+        }
+    }
+}
