3fee84033718d5b202dad76b29f9e68327f37805
Anil Mujagic <anil.mujagic@gmail.com>
2012-09-08 07:06:14 +0200

    Setup unit testing with xUnit

---
+++ Insula.Common.Tests/StringExtensionsTests.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Xunit;
+using Insula.Common;
+
+namespace Insula.Common.Tests
+{
+    public class StringExtensionsTests
+    {
+        [Fact]
+        public void CleanSpacesTest()
+        {
+            Assert.Equal<string>("My dirty string", " My dirty  string  ".CleanSpaces());
+        }
+    }
+}
