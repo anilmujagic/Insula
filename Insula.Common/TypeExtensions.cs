2bb2e4d047ea8c11b505f6643e387acff9895cbf
Anil Mujagic <anil.mujagic@gmail.com>
2012-10-22 01:01:16 +0200

    Get default value for a type

---
+++ Insula.Common/TypeExtensions.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Insula.Common
+{
+    public static class TypeExtensions
+    {
+        public static object GetDefault(this Type type)
+        {
+            if (type.IsValueType)
+                return Activator.CreateInstance(type);
+
+            return null;
+        }
+    }
+}
